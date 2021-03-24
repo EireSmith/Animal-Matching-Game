e14c859dad516813f90eb1f9d4c8ec9aa7d2016b
Rob <71661538+EireSmith@users.noreply.github.com>
2021-03-24 16:17:30 +0000

    Added Timer

--- Animal Matching Game/MainWindow.xaml.cs
+++ Animal Matching Game/MainWindow.xaml.cs
@@ -15,21 +15,40 @@ using System.Windows.Shapes;
 
 namespace Animal_Matching_Game
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
 
         }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = "Finished in " + timeTextBlock.Text + ". Play Again?";
+            }
+        }
+
         private void SetUpGame()
         {
-            List<string> animalEmoji = new List<string>()
+            List<string> animalEmoji = new List<string>()  // creates a list of emojis
             { 
                 "🐉", "🐉",
                 "🐍", "🐍",
@@ -41,19 +60,62 @@ namespace Animal_Matching_Game
                 "🐇", "🐇",
             };
 
-            Random random = new Random();
+            Random random = new Random(); // creates a random number generator
 
-            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())  //goes through each textblock in the grid
             {
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);            //creates a random number within the list count
+                    string nextEmoji = animalEmoji[index];          //selects the emoji from the list at the index of the random number 
+                    textBlock.Text = nextEmoji;                     // replaces the textblock text with the randomly indexed emoji
+                    animalEmoji.RemoveAt(index);    // removes the emoji from the list.
+                }
             
             }
 
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
 
 
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e) // when TextBlock is clicekd
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+            if(findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)  //if current TextBlcok content matches the last
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
+            
         }
     }
 }
